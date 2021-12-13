b5f9fe0293751e48640cd920c581e34731b98a02
nimeshakya <nimeshakyabtw@gmail.com>
2021-12-13 12:17:55 +0545

    all functions ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import QuoteContainer from './QuoteContainer';
 
-const colors = [
+const colorList = [
     'fab24b',
     '49c6e5',
     '90e0ef',
@@ -16,14 +16,52 @@ const colors = [
     'c81d25',
 ];
 
-const App = () => {
-    const [color, setColor] = useState(
-        colors[Math.floor(Math.random() * colors.length)]
-    );
+const url =
+    'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
+const App = () => {
+    const [loading, setLoading] = useState(true);
+    const [quotes, setQuotes] = useState([]);
+    const [color, setColor] = useState(
+        colorList[Math.floor(Math.random() * colorList.length)]
+    );
+    const [quote, setQuote] = useState({});
+
+    const fetchQuotes = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(url);
+            const quotes = await response.json();
+            setLoading(false);
+            setQuotes(quotes.quotes);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchQuotes();
+    }, []);
+
+    console.log(quotes);
+
+    useEffect(() => {
+        setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+    }, [quotes]);
+
+    console.log(quote);
     return (
-        <main style={{ 'background-color': `#${color}` }}>
-            <QuoteContainer color={color} setColor={setColor} />
+        <main style={{ backgroundColor: `#${color}` }}>
+            <QuoteContainer
+                color={color}
+                setColor={setColor}
+                colorList={colorList}
+                loading={loading}
+                quote={quote}
+                setQuote={setQuote}
+                quotes={quotes}
+            />
         </main>
     );
 };
--- src/QuoteContainer.js
+++ src/QuoteContainer.js
@@ -1,31 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const QuoteContainer = ({ color, setColor }) => {
+const QuoteContainer = ({
+    color,
+    setColor,
+    colorList,
+    loading,
+    quote,
+    setQuote,
+    quotes,
+}) => {
+    const handleClick = () => {
+        setColor(newColorFunc);
+        setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+    };
+
+    //function to avoid repeated color when choosing random color
+    const newColorFunc = () => {
+        let newColor = colorList[Math.floor(Math.random() * colorList.length)];
+        // if new color is same as previous then add its index by 1 and return that color
+        if (
+            newColor === color &&
+            colorList.indexOf(newColor) !== colorList.length - 1
+        ) {
+            newColor = colorList[colorList.indexOf(newColor) + 1];
+            return newColor;
+        } else if (
+            // if the color is last item in array then return first item
+            newColor === color &&
+            colorList.indexOf(newColor) === colorList.length - 1
+        ) {
+            newColor = colorList[0];
+        } else {
+            return newColor;
+        }
+    };
+
     return (
         <div className='quote-container'>
-            <div className='quote-div'>
-                <h1 style={{ color: `#${color}` }}>
-                    <span>
-                        <i class='fas fa-quote-left'></i>
-                    </span>
-                    Positive anything is better than negative nothing.
-                </h1>
-                <h3 style={{ color: `#${color}` }}>-Elbert Hubbard</h3>
-            </div>
+            {loading || quote === undefined ? (
+                <div className='quote-div-loading'>
+                    <h1 style={{ color: `#${color}` }}>
+                        <span>
+                            <i className='fas fa-quote-left'></i>
+                        </span>
+                    </h1>
+                </div>
+            ) : (
+                <div className='quote-div'>
+                    <h1 style={{ color: `#${color}` }}>
+                        <span>
+                            <i className='fas fa-quote-left'></i>
+                        </span>
+                        {quote.quote}
+                    </h1>
+                    <h3 style={{ color: `#${color}` }}>-{quote.author}</h3>
+                </div>
+            )}
+
             <div className='controls-div'>
                 <ul className='social-container'>
-                    <li style={{ 'background-color': `#${color}` }}>
-                        <a href='#' className='social-link'>
-                            <i class='fab fa-twitter'></i>
+                    <li style={{ backgroundColor: `#${color}` }}>
+                        <a
+                            // link to post to twitter
+                            href={
+                                quote !== undefined &&
+                                `https://www.twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent(
+                                    `"${quote.quote}"\n- ${quote.author}`
+                                )}`
+                            }
+                            target='_blank'
+                            className='social-link'
+                        >
+                            <i className='fab fa-twitter'></i>
                         </a>
                     </li>
-                    <li style={{ 'background-color': `#${color}` }}>
-                        <a href='#' className='social-link'>
-                            <i class='fab fa-facebook'></i>
+                    <li style={{ backgroundColor: `#${color}` }}>
+                        <a
+                            // link to post to twitter
+                            href={
+                                quote !== undefined &&
+                                `https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${encodeURIComponent(
+                                    quote.author
+                                )}&content=${encodeURIComponent(
+                                    quote.quote
+                                )}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`
+                            }
+                            target='_blank'
+                            className='social-link'
+                        >
+                            <i className='fab fa-tumblr'></i>
                         </a>
                     </li>
                 </ul>
-                <button style={{ 'background-color': `#${color}` }}>
+                <button
+                    style={{ backgroundColor: `#${color}` }}
+                    onClick={handleClick}
+                >
                     Next Quote
                 </button>
             </div>
